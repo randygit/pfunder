21b67e573aa96a74c0c91ce593dd208ef7cc9530
catetcpasswd <randy.gonzales@gmail.com>
2013-11-07 22:28:53 +0800

    used user.find instead of user.findone to consider 2 email accounts were added but only one was verified

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,8 @@ var mongoose = require('mongoose'),
 
 module.exports = function(passport) {
     //Serialize sessions
+    // have to modify this for the correct user
+
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
@@ -19,31 +21,65 @@ module.exports = function(passport) {
     });
 
     //Use local strategy
+    //modify since the db could have several records
+    //of the same email but the only the verified account is valid
+
     passport.use(new LocalStrategy({
             usernameField: 'email',
             passwordField: 'password'
         },
         function(email, password, done) {
             console.log("passport.local ");
-            User.findOne({
-                email: email
-            }, function(err, user) {
-                if (err) {
+            User.find({ 'email': email, verified: true }, 
+                function(err,user) {
+
+                if(err) {
                     return done(err);
                 }
-                if (!user) {
+
+                console.log('User             ' + JSON.stringify(user)); 
+                console.log('User.length      ' + user.length);
+                console.log('User.email       ' + user[0].email);
+                console.log('User.disabled    ' + user[0].disabled);
+                console.log('User.deactivated ' + user[0].deactivated);
+
+                if(user.length == 0) {
                     console.log("passport.local unknown user");
                     return done(null, false, {
                         message: 'Passport.local: Unknown user'
-                    });
+                    });              
                 }
-                if (!user.authenticate(password)) {
-                    console.log("passport.local invalid password");
-                    return done(null, false, {
-                        message: 'Passport.local: Invalid password'
-                    });
+                if(user.length == 1) {
+                    console.log('passport.local check for password');
+                     
+                    // check for valid password
+                    if (!user[0].authenticate(password)) {
+                        console.log("passport.local invalid password");
+                        return done(null, false, {
+                            message: 'Passport.local: Invalid password.'
+                        });
+                    }
+                    console.log('passport.local check if disabled ' + user[0].disabled);
+
+                    //check if disabled is true
+                    if (user[0].disabled) {
+                        console.log("passport.local account is disabled");
+                        return done(null, false, {
+                            message: 'Passport.local: account is disabled.'
+                        });
+                    }
+                    console.log('passport.local check if deactivated '+ user[0].deactivated);
+
+                    //check if deactivated is true
+                    if (user[0].deactivated) {
+                        console.log("passport.local account is deactivated");
+                        return done(null, false, {
+                            message: 'Passport.local: account is deactivated.'
+                        });
+                    }
+
                 }
-                return done(null, user);
+                return done(null, user[0]);
             });
         }
     ));
