42b08eb9fca6fbf6829a94fb3ce8621cc000c65b
catetcpasswd <randy.gonzales@gmail.com>
2014-02-05 21:29:29 +0800

    changes for openshift

--- config/env/all.js
+++ config/env/all.js
@@ -3,7 +3,8 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
     root: rootPath,
-    port: process.env.PORT || 3000,
+    port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    ip: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
     db: process.env.MONGOHQ_URL,
     mailer: {
       auth: {
