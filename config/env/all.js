1a8410c7dfd216d8158a5df2efa2b536aac4db76
randygit <randy@adamas.com.ph>
2014-02-06 19:22:27 +0800

    changed port to 8080

--- config/env/all.js
+++ config/env/all.js
@@ -3,7 +3,7 @@ rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
     root: rootPath,
-    port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
     db: process.env.MONGOHQ_URL,
     mailer: {
